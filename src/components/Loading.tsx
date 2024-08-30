fe8352b5125973e5831c96becc7d3ca08ef4b861
kevit-siddharth-singh <siddharth.singh@kevit.io>
2024-08-30 17:07:03 +0530

    Added Loading text in Loading Component

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,7 +1,8 @@
 const Loading = () => {
   return (
-    <div className="absolute top-[50%] left-[50%] ">
-      <span className="loading loading-spinner text-accent "></span>
+    <div className="absolute top-[50%] left-[50%] flex flex-col items-center gap-2">
+      <p className="font-medium text-white">Loading..</p>
+      <span className=" loading loading-spinner text-accent "></span>
     </div>
   );
 };
