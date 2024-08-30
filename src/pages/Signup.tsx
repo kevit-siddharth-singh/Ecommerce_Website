4080bb06aeb3c197812e340692a3376fafc9fea8
kevit-siddharth-singh <siddharth.singh@kevit.io>
2024-08-30 17:16:44 +0530

    changed some UI data

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -29,7 +29,7 @@ const Signup = () => {
               </a>
 
               <h2 className="mt-6 text-2xl font-bold text-white sm:text-3xl md:text-4xl">
-                Welcome to Squid 🦑
+                Welcome to Sky shop
               </h2>
 
               <p className="mt-4 leading-relaxed text-white/90">
@@ -61,7 +61,7 @@ const Signup = () => {
                 </a>
 
                 <h1 className="mt-2 text-2xl font-bold text-gray-900 sm:text-3xl md:text-4xl dark:text-white">
-                  Welcome to Squid 🦑
+                  Welcome to Sky shop.
                 </h1>
 
                 <p className="mt-4 leading-relaxed text-gray-500 dark:text-gray-400">
