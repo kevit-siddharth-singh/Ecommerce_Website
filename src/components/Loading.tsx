087f1d9b5b641862b85942de9cca492546b2fec8
kevit-siddharth-singh <siddharth.singh@kevit.io>
2024-08-30 17:12:21 +0530

    Fixed Bug in Loading component

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,8 +1,8 @@
 const Loading = () => {
   return (
-    <div className="absolute top-[50%] left-[50%] flex flex-col items-center gap-2">
+    <div className="absolute top-[50%] left-[50%] flex flex-col items-center gap-3">
       <p className="font-medium text-white">Loading..</p>
-      <span className=" loading loading-spinner text-accent "></span>
+      <span className=" loading loading-spinner loading-lg text-accent "></span>
     </div>
   );
 };
