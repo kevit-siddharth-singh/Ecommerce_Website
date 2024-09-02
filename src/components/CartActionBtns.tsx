4a0966f77e5bdb6264de87afd90052ac607911b6
kevit-siddharth-singh <siddharth.singh@kevit.io>
2024-09-02 14:46:33 +0530

    Added React-Icons and Cart btn-UI

--- src/components/CartActionBtns.tsx
+++ src/components/CartActionBtns.tsx
@@ -1,11 +1,16 @@
+import { FaRegHeart } from "react-icons/fa6";
+import { LuShoppingCart } from "react-icons/lu";
+
 const CartActionBtns = () => {
   return (
     <div className="btn-wrapper my-3 text-white flex gap-10">
-      <button className="bg-emerald-500 py-3 px-10 rounded text-xl font-medium">
+      <button className="bg-emerald-500 flex justify-center items-center gap-3 py-3 px-10 rounded text-xl font-medium active:bg-emerald-600">
+        <LuShoppingCart />
         Add to cart
       </button>
       <div>
-        <button className="bg-white py-3 px-10 rounded text-xl font-medium">
+        <button className="bg-red-500 flex justify-center items-center gap-3  py-3 px-10 rounded text-xl font-medium active:bg-red-600">
+          <FaRegHeart />
           Add to wishlist
         </button>
       </div>
