82893606e3e839ed874d00618e2e26c7b564ee47
kevit-siddharth-singh <siddharth.singh@kevit.io>
2024-09-05 11:53:30 +0530

    added Go to cart btn in ProductDetail.tsx

--- src/pages/ProductDetail.tsx
+++ src/pages/ProductDetail.tsx
@@ -1,14 +1,15 @@
 import { useQuery } from "@tanstack/react-query";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import Loading from "../components/Loading";
 import { getProductDetail } from "../utils/getProductDetail";
 import BreadCrumbs from "../components/BreadCrumbs";
 import Rating from "../components/Rating";
 import CartActionBtns from "../components/CartActionBtns";
+import { FiShoppingCart } from "react-icons/fi";
 
 const ProductDetail = () => {
   const { id } = useParams();
-  console.log(id);
+  const navigate = useNavigate();
   const { data } = useQuery({
     queryKey: ["products", { productId: id }],
     queryFn: () => getProductDetail(id!),
@@ -21,7 +22,15 @@ const ProductDetail = () => {
         <img className=" rounded-box  h-full w-full" src={data.image} alt="" />
       </div>
       <div className="w-[45rem] product-data flex flex-col gap-5">
-        <BreadCrumbs product={data.title} />
+        <div className="flex justify-between items-center">
+          <BreadCrumbs product={data.title} />
+          <button
+            onClick={() => navigate("/product/cart")}
+            className="text-white text-2xl  bg-red-500 p-2 px-4  active:bg-red-600 rounded"
+          >
+            <FiShoppingCart />
+          </button>
+        </div>
         <div className="product-title cursor-pointer text-white text-5xl p-1 font-bold truncate  ">
           {data.title}
         </div>
