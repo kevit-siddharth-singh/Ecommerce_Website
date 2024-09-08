0b2b6339cfa80028f5561581383b4a728462acf7
siddhu1919 <siddharthsingh1071@gmail.com>
2024-09-08 22:29:22 +0530

    chore:Add responsiveness to Product detail page

--- src/components/ProductCarousel.tsx
+++ src/components/ProductCarousel.tsx
@@ -1,14 +1,14 @@
 const ProductCarousel: React.FC<{ img: string }> = ({ img }) => {
   return (
     <>
-      <div className="carousel w-full h-full rounded-box bg-red-500">
+      <div className="carousel w-full h-full rounded-box bg-white">
         <div id="item1" className="carousel-item w-full">
-          <img src={img} className="w-full  object-cover" />
+          <img src={img} className="w-full  object-contain" />
         </div>
         <div id="item2" className="carousel-item w-full">
           <img
             src="https://avatars.githubusercontent.com/u/85176765?v=4"
-            className="w-full object-cover"
+            className="w-full object-contain"
           />
         </div>
         <div id="item3" className="carousel-item w-full">
@@ -17,7 +17,7 @@ const ProductCarousel: React.FC<{ img: string }> = ({ img }) => {
         <div id="item4" className="carousel-item w-full">
           <img
             src="https://img.daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.webp"
-            className="w-full object-cover"
+            className="w-full object-contain"
           />
         </div>
       </div>
--- src/components/Rating.tsx
+++ src/components/Rating.tsx
@@ -6,7 +6,12 @@ const Rating: React.FC<{ rating: number }> = (props) => {
       {Array(props.rating)
         .fill(0)
         .map((_, index) => (
-          <img key={index} className="max-md:w-4 max-md:mt-1  lg:w-[1.2rem] " src={star} alt="star" />
+          <img
+            key={index}
+            className="max-md:w-4 max-md:mt-1 md:w-5  lg:w-[1.2rem] "
+            src={star}
+            alt="star"
+          />
         ))}
     </div>
   );
